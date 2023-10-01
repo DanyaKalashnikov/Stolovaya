bca28b571eb31974937643a5cb94cec9dad3bfbc
Danya <lazertagpros@gmail.com>
2023-10-01 18:54:07 +0300

    package utils;
    public interface Demonstrate {
        void demonstrate();
    }

---
+++ src/utils/Menu.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Menu {
+    public static void showMainMenu(){
+        System.out.println("Выберите пункт: ");
+        System.out.println("    1. Показать:");
+        System.out.println("        11. ...");
+        System.out.println("        12. ...");
+        System.out.println("        13. ...");
+        System.out.println("    2. Добавить: ");
+        System.out.println("        21. ...");
+        System.out.println("        22. ...");
+        System.out.println("        23. ...");
+        System.out.println("    3. Выйти");
+    }
+}
